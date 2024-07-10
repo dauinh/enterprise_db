447b9482df05490a738f51339cb171c2ad77d23f
linh <linh.ha.nguyen20@gmail.com>
2024-07-10 16:00:47 -0700

    refactor save collections

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,6 +4,9 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from storage import CVSStorage
+
+
 class Crawler:
     def __init__(self):
         self.driver = webdriver.Firefox()
@@ -12,7 +15,7 @@ class Crawler:
         self.driver.get(url)
         return self.driver.page_source
 
-    def get_all_collections_links(self, html):
+    def parse_collections_links(self, html):
         links = self.driver.find_elements(By.TAG_NAME, "a")
         results = []
         for l in links:
@@ -21,6 +24,13 @@ class Crawler:
             if x:
                 results.append(url)
         return results
+    
+    def save_collections(self, links):
+        save_file = CVSStorage('data.collections.csv')
+        save_file.save(['No', 'Collection'])
+        for i, l in enumerate(links):
+            collection = l.split('/')[-1]
+            save_file.save([i+1, collection])
 
     def quit(self):
         self.driver.quit()
--- crawler/run.crawler.py
+++ crawler/run.crawler.py
@@ -1,19 +1,14 @@
 # scripts/run_crawler.py
 from crawler import Crawler
-from storage import CVSStorage
 
 if __name__ == '__main__':
     crawler = Crawler()
     try:
         url = 'https://www.muji.us/collections/'
         html = crawler.fetch(url)
-        links = crawler.get_all_collections_links(html)
+        links = crawler.parse_collections_links(html)
+        crawler.save_collections(links)
         
-        save_file = CVSStorage('data.collections.csv')
-        save_file.save(['No', 'Collection'])
-        for i, l in enumerate(links):
-            collection = l.split('/')[-1]
-            save_file.save([i+1, collection])
     except Exception as e:
         print(e)
     finally:
