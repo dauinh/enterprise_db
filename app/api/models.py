7ca45266da3a7ec7eb4b4edcc729d3f9f2a283fc
linh <linh.ha.nguyen20@gmail.com>
2024-08-01 18:29:01 -0700

    model attribute entity

--- app/api/models.py
+++ app/api/models.py
@@ -1,17 +1,26 @@
 import uuid
 
 from sqlalchemy import Table, ForeignKey, Column, Integer, String, Boolean, Numeric
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import relationship
 
 from app.api.db import Base
 
 
 # Relationship table between Product and Collection
-belongs = Table(
-    "belongs",
+product_collection = Table(
+    "product_collection",
     Base.metadata,
     Column("product_id", Integer, ForeignKey("product.id")),
     Column("collection_id", Integer, ForeignKey("collection.id")),
+)
+
+
+# Relationship table between Product and Attribute
+product_attribute = Table(
+    "product_attribute",
+    Base.metadata,
+    Column("product_id", Integer, ForeignKey("product.id")),
+    Column("attribute_id", Integer, ForeignKey("attribute.id")),
 )
 
 
@@ -26,7 +35,10 @@ class Product(Base):
     is_active = Column(Boolean)
     total_quantity = Column(Integer)
     collections = relationship(
-        "Collection", secondary=belongs, back_populates="products"
+        "Collection", secondary=product_collection, back_populates="products"
+    )
+    attributes = relationship(
+        "Attribute", secondary=product_attribute, back_populates="products"
     )
 
 
@@ -37,4 +49,15 @@ class Collection(Base):
         Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
     )
     name = Column(String(150))
-    products = relationship("Product", secondary=belongs, back_populates="collections")
+    products = relationship("Product", secondary=product_collection, back_populates="collections")
+
+
+class Attribute(Base):
+    __tablename__ = "attribute"
+
+    id = Column(
+        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
+    )
+    products = relationship("Product", secondary=product_attribute, back_populates="attributes")
+    color = Column(String(50))
+    size = Column(String(50))
