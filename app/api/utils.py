fb69ff2649d7a1eea658e8ecada0e710f6dea90c
linh <linh.ha.nguyen20@gmail.com>
2024-08-06 20:59:32 -0700

    insert collections

--- app/api/utils.py
+++ app/api/utils.py
@@ -3,17 +3,17 @@ import csv
 
 from sqlalchemy.orm import sessionmaker
 
-from .models import Product
-from .db import engine, SessionLocal, Base
+from app.api.db import engine, SessionLocal, Base
+from app.api.models import Product, Collection
 
 
 def insert_products(Session: sessionmaker) -> None:
-    file_path = sys.path[0] + "/../data/products.csv"
+    file_path = sys.path[0] + "/../data/product.csv"
     data = []
     with open(file_path, "r") as f:
         reader = csv.DictReader(f)
-        next(reader)
-        for row in reader:
+        for i, row in enumerate(reader):
+            row['id'] = i + 1
             try:
                 row["current_price"] = (
                     float(row["current_price"]) if row["current_price"] else 0.0
@@ -21,15 +21,15 @@ def insert_products(Session: sessionmaker) -> None:
             except ValueError:
                 row["current_price"] = 0.0
 
-            try:
-                row["color"] = row["color"] if row["color"] else ""
-            except ValueError:
-                row["color"] = ""
+            # try:
+            #     row["color"] = row["color"] if row["color"] else ""
+            # except ValueError:
+            #     row["color"] = ""
 
-            try:
-                row["size"] = row["size"] if row["size"] else ""
-            except ValueError:
-                row["size"] = ""
+            # try:
+            #     row["size"] = row["size"] if row["size"] else ""
+            # except ValueError:
+            #     row["size"] = ""
 
             try:
                 row["is_active"] = bool(row["is_active"])
@@ -51,7 +51,29 @@ def insert_products(Session: sessionmaker) -> None:
                 session.rollback()
 
 
+def insert_collections(Session: sessionmaker) -> None:
+    file_path = sys.path[0] + "/../data/collection.csv"
+    data = []
+    with open(file_path, "r") as f:
+        reader = csv.DictReader(f)
+        for i, row in enumerate(reader):
+            row['id'] = i + 1
+            data.append(row)
+
+    with Session as session:
+        for i, row in enumerate(data):
+            collection = Collection(**row)
+            session.add(collection)
+            try:
+                session.commit()
+            except Exception as e:
+                print(e)
+                print("Cannot insert", row["name"])
+                session.rollback()
+
+
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     db = SessionLocal()
-    insert_products(db)
+    # insert_products(db)
+    # insert_collections(db)
